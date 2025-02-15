bdc1e41577c208d48a528de4f40ba7cc504c4f01
Sohom Ghosh <sohom.official21@gmail.com>
2025-02-16 00:10:38 +0530

    Update Story.jsx

--- frontend/src/Components/LandingPage_components/Story.jsx
+++ frontend/src/Components/LandingPage_components/Story.jsx
@@ -44,7 +44,7 @@ const Story = () => {
   return (
     <section id='story' className='min-h-dvh w-screen bg-black text-blue-50'>
          <div className='flex size-full flex-col items-center py-10 pb-24'>
-             <p className='font-general text-sm uppercase md:text-[10px]'>Lorem ipsum dolor sit.</p>
+             <p className='font-general text-sm uppercase md:text-[10px]'>Welcome to Rama Realm</p>
              <div className='relative size-full'>
                   <AnimatedTitle
                    title="A Uni<b>q</b>ue World <br />where lea<b>r</b>ning Comes Alive"
@@ -71,7 +71,7 @@ const Story = () => {
              </div>
              <div className='-mt-80 flex w-full justify-center md:-mt-64 md:me-44 md:justify-end'>
                 <div className='flex h-full w-fit flex-col items-center md:items-start'>
-                  <p className='mt-3 max-w-sm text-center font-circular-web text-violet-50 md:text-start'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia minus tenetur vero?</p>
+                  <p className='mt-3 max-w-sm text-center font-circular-web text-violet-50 md:text-start'>RamaRealm is a place where learning comes alive. We believe in the power of learning and the transformative potential of technology.</p>
                   <Button
                   id="realm-button"
                   title={"discover New Things"}
