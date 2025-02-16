fb795076b697924d39603ddebd4cc350e324fe7f
ritwickrajmakhal <ritwickrajmakhal11@gmail.com>
2025-02-16 05:59:16 +0530

    Fix import casing for LearnerNavbar component

--- frontend/src/pages/Learner/LearnerHomepage.jsx
+++ frontend/src/pages/Learner/LearnerHomepage.jsx
@@ -1,9 +1,9 @@
 //A9hWTFu44MeqAYa8
 import Courses from '@/Components/LearnerComponents/Courses';
 import Footer from '@/Components/LearnerComponents/Footer_learner';
-import Learnernavbar from '@/Components/LearnerComponents/LearnerNavbar';
+import Learnernavbar from '@/Components/LearnerComponents/Learnernavbar';
 import Learner_hero from '@/Components/LearnerComponents/Learner_hero';
-import { Router } from 'react-router-dom';
+
 const LearnerHomepage = () => {
   return (
     <>
